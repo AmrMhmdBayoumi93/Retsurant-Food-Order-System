d4a7eea168364ee24b2c3e3484296936d420c948
Amr Muhammad <amb600680@gmail.com>
2026-05-13 11:11:12 +0300

    finish auth module Back-end Amr

---
+++ Back-End/Cravely/Util/jwt.js
@@ -0,0 +1,11 @@
+const jwt = require("jsonwebtoken");
+
+exports.generateToken = (userId, userRole) => {
+  const token = jwt.sign(
+    { id: userId, role: userRole },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  return token;
+};
--- Back-End/Cravely/app.js
+++ Back-End/Cravely/app.js
@@ -1,1 +1,40 @@
-const express = require("express");
+  
+  //.env
+ const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+  //express
+    const express = require('express');
+    const app = express();
+  const mongoose = require('mongoose');
+
+  //middleware
+  app.use(express.json());  // to build rest api and to parse json data from the request body
+
+  
+
+  
+  //port
+      // const PORT = process.env.PORT || 5000;
+       const port = process.env.PORT;
+
+  //db connection
+  
+async function DB_connection() {
+  try {
+    await mongoose.connect(process.env.DB_URL);
+    console.log("DataBase connected successfully");
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+  DB_connection();
+  //run the server 
+
+  app.listen(port, () => {
+  console.log(`Server is connected on ${port}`);
+});
+
+
+
+
